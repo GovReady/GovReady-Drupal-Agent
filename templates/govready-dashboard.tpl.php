aa825921ad26ba71db1c0f8aa312757b2c116d3b
Alex Schmoe <aschmoe@gmail.com>
2016-07-14 07:25:01 -0700

    removing extra php closing

--- templates/govready-dashboard.tpl.php
+++ templates/govready-dashboard.tpl.php
@@ -5,7 +5,6 @@
  * Template for the GovReady Dashboard.
  */
 ?>
-?>
 <div class="container"><div class="col-xs-12">
   <h1 style="line-height: 65px;margin: 10px 0;"><a href="#" style="text-decoration: none !important;">
     <img src="<?php print $logo ?>" style="height:50px;float:left;margin-right:10px;" alt="GovReady" />
